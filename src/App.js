8aa11a6c503dd94118156344bef57458411fc9cf
Julian <julian.fcp@gmail.com>
2022-04-06 19:30:13 -0500

    Github pages update

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const [libraryStatus, setLibraryStatus] = useState(false);
 
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
       <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
